0df740883d583f3f2553928fbff2f8ae19f21548
LPRegen <manuel.escribano.051@gmail.com>
2023-02-25 06:03:56 -0300

    refactor: code style

--- mappings.lua
+++ mappings.lua
@@ -6,8 +6,8 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>bv"] = { ":vsplit <CR>", "split buffer vertically" },
     ["<leader>qq"] = { ":qa! <CR>", "Close all files and exit nvim" },
-    ["<A-j>"] = {":m .+1<CR>", "Move line down"},
-    ["<A-k>"] = {":m .-2<CR>", "Move line up"},
+    ["<A-j>"] = { ":m .+1<CR>", "Move line down" },
+    ["<A-k>"] = { ":m .-2<CR>", "Move line up" },
   },
 }
 
