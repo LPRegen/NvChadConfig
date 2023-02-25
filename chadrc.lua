2c24752c51fe93736fe05d078c6624d28d1c2c31
LPRegen <manuel.escribano.051@gmail.com>
2023-02-25 20:14:22 -0300

    refactor: change default themes

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme_toggle = { "onedark", "one_light" },
-  theme = "tokyonight",
+  theme_toggle = { "ayu-dark", "tomorrow_night" },
+  theme = "ayu-dark",
   hl_override = highlights.override,
   hl_add = highlights.add,
 
